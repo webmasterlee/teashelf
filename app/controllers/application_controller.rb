c1b7ab9a957457f606660b73d8ba72ae5a51c411
webmasterlee <webmasterlee@live.com>
2016-03-07 13:44:31 -0500

    bigger icons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,12 @@ class ApplicationController < ActionController::Base
   def check_night_mode
 
     if user_signed_in? && session[:night_mode].nil?
+      session[:night_mode] = "off"
+
       @night_mode = Preference.find_or_initialize_by :user_id => current_user.id, name: "night_mode_default"
       
       unless @night_mode.nil?
-        session[:night_mode] = @night_mode.value == "true" ? "on" : ""
+        session[:night_mode] = @night_mode.value == "true" ? "on" : "off"
       end
     end
 

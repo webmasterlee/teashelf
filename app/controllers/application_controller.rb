3adf927304a869801fc0524a7c5e6a8c5afd63b8
webmasterlee <webmasterlee@live.com>
2016-03-07 08:57:59 -0500

    updated night mode check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,15 @@ class ApplicationController < ActionController::Base
   end
 
   def check_night_mode
+
+    if user_signed_in?
+      @night_mode = Preference.find_or_initialize_by :user_id => current_user.id, name: "night_mode_default"
+      
+      unless @night_mode.nil?
+        session[:night_mode] = @night_mode.value == "true" ? "on" : ""
+      end
+    end
+    
   	if params[:night_mode]
 	  	session[:night_mode] = params[:night_mode]
 	   end
--- app/controllers/my_devise/sessions_controller.rb
+++ app/controllers/my_devise/sessions_controller.rb
@@ -11,17 +11,6 @@ class MyDevise::SessionsController < Devise::SessionsController
       end
     end
   end
-
-  def new 
-  	super
-  	 if user_signed_in?
-      @night_mode = Preference.find_or_initialize_by :user_id => current_user.id, name: "night_mode_default"
-      
-      unless @night_mode.nil?
-        session[:night_mode] = @night_mode.value == "true" ? "on" : ""
-      end
-    end
-  end
   
 
 end

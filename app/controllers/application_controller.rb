2e3b1edb82652fa7b90424e6455c7d9e7167cbfd
webmasterlee <webmasterlee@live.com>
2016-03-07 09:04:30 -0500

    added check for session night mode variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,14 @@ class ApplicationController < ActionController::Base
 
   def check_night_mode
 
-    if user_signed_in?
+    if user_signed_in? && session[:night_mode].nil?
       @night_mode = Preference.find_or_initialize_by :user_id => current_user.id, name: "night_mode_default"
       
       unless @night_mode.nil?
         session[:night_mode] = @night_mode.value == "true" ? "on" : ""
       end
     end
-    
+
   	if params[:night_mode]
 	  	session[:night_mode] = params[:night_mode]
 	   end

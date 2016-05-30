a134ec9f32d2efcff5137eb8f88e4a01cd65cf94
webmasterlee <webmasterlee@live.com>
2016-05-30 12:39:00 -0400

    try new gem for 404 assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Teashelf
   class Application < Rails::Application
+    config.assets.precompile += %w[404.html 422.html 500.html] #for error_page_assets gem
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
